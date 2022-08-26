69b892021a33b97249e3a1d6a36d65be43edccea
Shanic0120 <101580943+Shanic0120@users.noreply.github.com>
2022-08-25 21:19:15 -0500

    Add files via upload

---
+++ televisores/control.py
@@ -0,0 +1,34 @@
+class Control:
+    def __init__(self):
+        self.__tv=None
+
+    def turnOn(self):
+        self.__tv.turnOn()
+
+    def turnOff(self):
+        self.__tv.turnOff()
+
+    def setCanal(self,canal):
+        self.__tv.setCanal(canal)
+
+    def canalUp(self):
+        self.__tv.canalUp()
+        
+    def canalDown(self):
+        self.__tv.canalDown()
+
+    def volumenUp(self):
+        self.__tv.volumenUp()
+        
+    def volumenDown(self):
+        self.__tv.volumenDown()
+
+    def enlazar(self,tv):
+        self.__tv=tv
+        tv.setControl(self)
+
+    def getTv(self):
+        return self.__tv
+
+    def setTv(self,tv):
+        self.__tv=tv
---
+++ televisores/tv.py
@@ -0,0 +1,83 @@
+from xml.etree.ElementTree import canonicalize
+
+
+class TV:
+    __numTV=0
+    def __init__(self,marca,estado):
+        self.__marca=marca
+        self.__canal=1
+        self.__precio=500
+        self.__estado=estado
+        self.__volumen=1
+        self.__control=None
+
+        TV.__numTV+=1
+    
+    def getMarca(self):
+        return self.__marca
+
+    def setMarca(self,marca):
+        self.__marca=marca
+    
+    def getControl(self):
+        return self.__control
+
+    def setControl(self,control):
+        self.__control=control
+    
+    def getPrecio(self):
+        return self.__precio
+
+    def setPrecio(self,precio):
+        self.__precio=precio
+    
+    def getVolumen(self):
+        return self.__volumen
+
+    def setVolumen(self,volumen):
+        if self.__estado and volumen>=0 and volumen<=7:
+            self.__volumen=volumen
+    
+    def getCanal(self):
+        return self.__canal
+
+    def setCanal(self,canal):
+        if self.__estado and canal>=1 and canal<=120:
+            self.__canal=canal
+
+    @classmethod
+    def getNumTV(cls):
+        return cls.__numTV
+        
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls.__numTV=numTV
+    
+    def turnOn(self):
+        self.__estado=True
+
+    def turnOff(self):
+        self.__estado=False
+    
+    
+
+
+    def canalUp(self):
+        if self.__estado and self.__canal<120:
+            self.__canal+=1
+        
+    def canalDown(self):
+        if self.__estado and self.__canal>1:
+            self.__canal-=1
+
+    def volumenUp(self):
+        if self.__estado and self.__volumen<7:
+            self.__canal+=1
+        
+    def volumenDown(self):
+        if self.__estado and self.__volumen>0:
+            self.__volumen-=1
+
+
+    def getEstado(self):
+        return self.__estado
